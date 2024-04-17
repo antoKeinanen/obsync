6a52062f335502d3717304d40ffd7831677bbec3
antoKeinanen <anto.keinanen@gmail.com>
2024-04-17 11:40:03 +0300

    full common mark spec support

--- src/lib/markdown.tsx
+++ src/lib/markdown.tsx
@@ -1,19 +1,41 @@
-import sanitizeHtml from "sanitize-html";
-import { RMark } from "./parser/rules";
+import { alert } from "@mdit/plugin-alert";
+import markdownIt from "markdown-it";
 
 interface MarkdownProps {
   children: string;
 }
 
-
 async function Markdown({ children }: MarkdownProps) {
-  const dirty_parsed = new RMark().render(children);
-  const clean_parsed = sanitizeHtml(dirty_parsed);
+  const mdIt = markdownIt().use(alert, {
+    alertNames: [
+      "abstract",
+      "summary",
+      "tldr",
+      "info",
+      "note",
+      "todo",
+      "tip",
+      "success",
+      "question",
+      "help",
+      "faq",
+      "warning",
+      "caution",
+      "attention",
+      "failure",
+      "fail",
+      "missing",
+      "bug",
+      "example",
+      "quote",
+    ],
+  });
+  const parsed = mdIt.render(children);
 
   return (
     <article
       className="break-words"
-      dangerouslySetInnerHTML={{ __html: clean_parsed }}
+      dangerouslySetInnerHTML={{ __html: parsed }}
     ></article>
   );
 }
--- src/lib/parser/types/Pattern.ts
+++
@@ -1,7 +0,0 @@
-export class Pattern {
-  constructor(public regex: RegExp, public replacement: string) {}
-
-  apply(raw: string): string {
-    return raw.replace(this.regex, this.replacement);
-  }
-}
